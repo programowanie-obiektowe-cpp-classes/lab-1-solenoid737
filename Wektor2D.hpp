4247bd6e3d696a61d2b60444f729ac8861eef909
Hubert Piwowarski <01150042@pw.edu.pl>
2025-10-22 14:25:55 +0200

    zejsciowka

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,49 @@
+#include <math.h>
+//#include <print>
+
 class Wektor2D
 {
+public:
     // Tutaj napisz implementacje klasy Wektor2D
+    Wektor2D() // konstruktor domyslny
+    {
+        x = 0;
+        y = 0;
+        //std::println("wspolrzedne: x = {}, y = {}", x, y);
+    };
+
+    Wektor2D(double a, double b) // konstruktor
+    {
+        x = a;
+        y = b;
+        //std::println("wspolrzedne: x = {}, y = {}", x, y);
+    };
+
+    ~Wektor2D() // destruktor
+    {};
+
+    void setX(int a) { x = a; };
+    void setY(int b) { y = b; };
+
+    int getX() { return x; };
+    int getY() { return y; };
+
+    double x;
+    double y;
+};
+
+Wektor2D operator+(Wektor2D v1, Wektor2D v2)
+{
+    double sumx, sumy;
+    sumx = v1.getX() + v2.getX();
+    sumy = v1.getY() + v2.getY();
+
+    return Wektor2D{sumx, sumy};
+};
+
+double operator*(Wektor2D v1, Wektor2D v2)
+{
+    double scalar;
+    scalar = v1.getX() * v2.getX() + v1.getY() * v2.getY();
+    return scalar;
 };
